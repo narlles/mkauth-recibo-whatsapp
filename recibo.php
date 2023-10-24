196d68a482a2044d632ca95a97d05d846debda09
Mauricio Ferreira Sarmento <maumau10-@hotmail.com>
2023-10-24 19:07:18 -0300

    Adicionando comentarios

--- recibo.php
+++ recibo.php
@@ -51,7 +51,7 @@ if(isset($_POST["titulo"])){
 $titulo=$_POST["titulo"];
 if(isset($_POST["chave"])){
 $chave=$_POST["chave"];}
-if($chave == 'chave-segurança'){
+if($chave == 'chave-segurança'){ # <---- COLOQUE AQUI SUA CHAVE DE SEGURANÇA
 # CONEXÃO COM O BANCO DE DADOS DO MK-AUTH
 $host = "localhost";
 $usuario = "root";
@@ -63,21 +63,21 @@ echo "Falha na conexão: (".$mysqli->connect_errno.") ".$mysqli->connect_error;
 $con = mysqli_connect("$host","$usuario","$senha");
 mysqli_select_db($con,"$db");
 
-# PUXAR OS DADOS DO CLIENTE
+# PUXAR OS DADOS DO BOLETO NO BANCO DO MK-AUTH
 $boleto="SELECT datavenc, datapag, valor, valorpag, coletor, formapag, login FROM sis_lanc where id = $titulo";
 $res= mysqli_query($con,$boleto);
-#$lin=mysqli_num_rows($res);
 while($vreg=mysqli_fetch_row($res)){
- $datavenc = $vreg[0]; echo '<br>';
- $datapag = $vreg[1]; echo '<br>';
- $valor = $vreg[2]; echo '<br>';
- $valorpag = $vreg[3]; echo '<br>';
- $coletor = $vreg[4]; echo '<br>';
- $formapag = $vreg[5]; echo '<br>';
- $login = $vreg[6]; echo '<br>';
+# PODE PEGAR QUALQUER DADOS QUE QUISEREM, ESTES FOI OS QUE O CIRO QUIS PEGAR
+ $datavenc = $vreg[0];
+ $datapag = $vreg[1];
+ $valor = $vreg[2];
+ $valorpag = $vreg[3];
+ $coletor = $vreg[4];
+ $formapag = $vreg[5];
+ $login = $vreg[6];
 }
 
-# PUXAR OS DADOS DO CLIENTE
+# PUXAR O NUMERO DO CLIENTE, QUE PODE PUCHAR MAS DADOS, COMO NA MINHA OPINIÃO, TERIA QUE PEGAR O NOME
 $boleto="SELECT celular FROM sis_cliente where login = '$login'";
 $res= mysqli_query($con,$boleto);
 #$lin=mysqli_num_rows($res);
@@ -85,11 +85,10 @@ while($vreg=mysqli_fetch_row($res)){
  $celular = $vreg[0]; echo '<br>';
 }
 
-
-// URL da EvolutionAPI
+# URL DA EvolutionAPI
 $apiUrl = 'http://{{baseURL}}/message/sendText/{{instance}}'; # DIGITE A URL AQUI
 
-// Dados 
+# DADOS 
 $data = array(
     "number" => "$celular",
     "options" => array(
@@ -97,8 +96,8 @@ $data = array(
         "presence" => "composing",
         "linkPreview" => false
     ),
-    "textMessage" => array(
-        "text" => "
+    "textMessage" => array( # MENSSAGEM QUE É ENVIADA PARA O CLIENTE NO WHATSAPP.
+        "text" => " 
 Mensagem Automatica de Recebimento de Pagamento
 Pagamento recebido em: *$datapag*
 Fatura com vencimento em: $datavenc
@@ -109,16 +108,16 @@ Forma de pagamento: $formapag
 
 Para segunda via e comprovantes dos pagamentos acesse:
 https://brlink.org/cliente ( Coloque o CPF do titular )
-"
+" # LEMBRAR DE COLOCAR A MENSSAGEM DENTRO DAS ASPAS, DETALHE, ACENTOS NÃO FUNCIONA, E QUANDO VOCÊ COPIA UM TEXTO E COLA, ELE TAMBÉM NÃO FUNCIONA, TEM QUE DIGITAR
 		)
 );
-// Converte JSON
+# Converte JSON
 $jsonData = json_encode($data);
 
-// Inicializa o cURL 
+# Inicializa o cURL 
 $ch = curl_init($apiUrl);
 
-// Configura cURL
+# Configura cURL
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, array(
     'Content-Type: application/json',
@@ -127,18 +126,18 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, array(
 curl_setopt($ch, CURLOPT_POST, true);
 curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
 
-// Executa
+# Executa
 $response = curl_exec($ch);
 
-// Verifica erro
+# Verifica erro
 if (curl_errno($ch)) {
     echo 'Erro ao chamar a API: ' . curl_error($ch);
 } else {
-    # echo 'Resposta da API: ' . $response;
+# echo 'Resposta da API: ' . $response;
       echo '<center>Menssagem enviada com sucesso!</center>';
 }
 
-// Feche seu cURL
+# Feche seu cURL
 curl_close($ch);
 }}
 ?>
